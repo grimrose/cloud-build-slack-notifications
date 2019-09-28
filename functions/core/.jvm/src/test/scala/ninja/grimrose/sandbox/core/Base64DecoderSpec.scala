3a874bdc48c5e57e2c47a9c14d070c475658f9cf
grimrose <grimrose@users.noreply.github.com>
2019-09-28 14:09:37 +0900

    Fix test

--- functions/core/.jvm/src/test/scala/ninja/grimrose/sandbox/core/Base64DecoderSpec.scala
+++ functions/core/.jvm/src/test/scala/ninja/grimrose/sandbox/core/Base64DecoderSpec.scala
@@ -1,6 +1,8 @@
 package ninja.grimrose.sandbox.core
 
-import io.scalajs.nodejs.buffer.Buffer
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
 import ninja.grimrose.sandbox.BaseSpecSupport
 import org.scalatest.FlatSpec
 
@@ -9,7 +11,7 @@ class Base64DecoderSpec extends FlatSpec with BaseSpecSupport {
   it should "be parsed" in {
     val source = "{\"xyz\":890}"
 
-    val input = Buffer.from(source, "utf-8").toString("base64")
+    val input = new String(Base64.getEncoder.encode(source.getBytes(StandardCharsets.UTF_8)))
 
     val actual = Base64Decoder.decode(input)
 
